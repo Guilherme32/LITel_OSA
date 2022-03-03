decda2d3a7e7954fcc5566eed05df2b29db26dd7
Guilherme32 <38697157+Guilherme32@users.noreply.github.com>
2022-03-03 11:12:50 -0300

    Adicionou um excepthook para facilitar debug

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
+from traceback import format_exception
 from PyQt6.QtWidgets import (
-    QMainWindow, QApplication,
+    QMessageBox, QApplication,
     QLabel, QToolBar, QStatusBar, QCheckBox
 )
 import os
@@ -9,6 +10,23 @@ from MainWindow import MainWindow, CONFIG_PATH
 
 if not os.path.isdir(os.path.dirname(CONFIG_PATH)):
     os.mkdir(os.path.dirname(CONFIG_PATH))
+
+
+def except_hook(type_, value, traceback):
+    sys.__excepthook__(type_, value, traceback)
+
+    dialog = QMessageBox()
+    dialog.setWindowTitle("Erro")
+
+    text = "".join(format_exception(type_, value, traceback))
+
+    dialog.setText(text)
+    dialog.exec()
+
+    sys.exit(-1)
+
+
+sys.excepthook = except_hook
 
 app = QApplication(sys.argv)
 w = MainWindow()
